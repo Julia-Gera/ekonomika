03e6e6aaeb46012ba1d6a19f9e8be0e944f4fe19
redevCourses <redev.courses@gmail.com>
2026-04-10 00:51:58 +0300

    Fix markdown renderer dependency for Vercel

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,9 +1,5 @@
 import 'server-only'
-
-// Reuse Strapi's markdown parser so article content from the CMS renders
-// consistently whether it is stored as markdown or already contains HTML.
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const MarkdownIt = require('../../backend/node_modules/markdown-it')
+import MarkdownIt from 'markdown-it'
 
 const markdown = new MarkdownIt({
   html: true,
